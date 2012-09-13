d87f30ecd7bf1efc4e334392ab1bddb3e0128288
Scott Rabin <scottrabin@gmail.com>
2012-09-12 21:52:46 -0700

    Better script/link asset behavior

--- _plugins/assets.rb
+++ _plugins/assets.rb
@@ -1,13 +1,31 @@
-require 'pathname'
+require 'uri'
 
 module Jekyll
 	module AssetFilter
+		def asset_location path
+			url = URI.parse path
+			if url.path.split('/').first == 'asset'
+				[
+					@context.registers[:site].config['baseurl'],
+					'assets',
+					url.path.split('/')[1..-1].join('/')
+				].select {|item| item.length > 0 }.join('/')
+			elsif url.path.split('/').first == 'vendor'
+				[
+					@context.registers[:site].config['baseurl'],
+					'vendor',
+					url.path.split('/')[1..-1].join('/')
+				].select {|item| item.length > 0 }.join('/')
+			else
+				url
+			end
+		end
 		def as_script input
-			%Q(<script type="text/javascript" src="#{Pathname.new(@context.registers[:site].config['baseurl']) + input}"></script>)
+			%Q(<script type="text/javascript" src="#{asset_location input}"></script>)
 		end
 
 		def as_stylesheet input
-			%Q(<link rel="stylesheet" type="text/css" href="#{Pathname.new(@context.registers[:site].config['baseurl']) + input}" />)
+			%Q(<link rel="stylesheet" type="text/css" href="#{asset_location input}" />)
 		end
 	end
 end
